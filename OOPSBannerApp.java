977e3e63bb8fee45939fe6742a0494dc015c9901
smanaspratap <manaspratap2003@gmail.com>
2026-02-06 14:59:15 +0530

    [Manas] Added: UC1: Print OOPS to Console.

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPS Banner App
+ * Prints OOPS to console.
+ *
+ * @author Devraj
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
