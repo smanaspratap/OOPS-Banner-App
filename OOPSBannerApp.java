f64e4acb994005e193b366d15e0c740d921fcc7d
smanaspratap <manaspratap2003@gmail.com>
2026-02-06 15:18:44 +0530

    [Manas] Added: UC4: Render OOPS as Banner using String Array and Loop.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,12 +4,18 @@ public class OOPSBannerApp {
         System.out.println("OOPS");
         System.out.println();
 
-        System.out.println(String.join("", "  *****  ", "   *****  ", " ******  ", "  *****  "));
-        System.out.println(String.join("", " *     * ", "  *     * ", " *     * ", " *       "));
-        System.out.println(String.join("", " *     * ", "  *     * ", " *     * ", " *       "));
-        System.out.println(String.join("", " *     * ", "  *     * ", " ******  ", "  *****  "));
-        System.out.println(String.join("", " *     * ", "  *     * ", " *       ", "       * "));
-        System.out.println(String.join("", " *     * ", "  *     * ", " *       ", "       * "));
-        System.out.println(String.join("", "  *****  ", "   *****  ", " *       ", "  *****  "));
+        String[] banner = new String[7];
+
+        banner[0] = String.join("", "  *****  ", "   *****  ", " ******  ", "  *****  ");
+        banner[1] = String.join("", " *     * ", "  *     * ", " *     * ", " *       ");
+        banner[2] = String.join("", " *     * ", "  *     * ", " *     * ", " *       ");
+        banner[3] = String.join("", " *     * ", "  *     * ", " ******  ", "  *****  ");
+        banner[4] = String.join("", " *     * ", "  *     * ", " *       ", "       * ");
+        banner[5] = String.join("", " *     * ", "  *     * ", " *       ", "       * ");
+        banner[6] = String.join("", "  *****  ", "   *****  ", " *       ", "  *****  ");
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
