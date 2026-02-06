931244e61e8285f450dc4bc33ebe8e9e2c3beb2f
smanaspratap <manaspratap2003@gmail.com>
2026-02-06 15:25:10 +0530

    [Manas] Added: UC6: Refactor Banner Logic into Functions.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,52 @@
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
         System.out.println("OOPS");
         System.out.println();
 
-        String[] banner = {
-                String.join("", "  *****  ", "   *****  ", " ******  ", "  *****  "),
-                String.join("", " *     * ", "  *     * ", " *     * ", " *       "),
-                String.join("", " *     * ", "  *     * ", " *     * ", " *       "),
-                String.join("", " *     * ", "  *     * ", " ******  ", "  *****  "),
-                String.join("", " *     * ", "  *     * ", " *       ", "       * "),
-                String.join("", " *     * ", "  *     * ", " *       ", "       * "),
-                String.join("", "  *****  ", "   *****  ", " *       ", "  *****  ")
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(String.join("", o[i], " ", o[i], " ", p[i], " ", s[i]));
         }
+    }
+
+    private static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    private static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    private static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " *       ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "       * ",
+                "  *****  "
+        };
     }
 }
